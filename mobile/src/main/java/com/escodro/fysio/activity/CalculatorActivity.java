1e733bc4de38a5631f0d8cd24e83ca7198ad4867
IgorEscodro <escodro@outlook.com>
2015-11-22 10:11:17 -0200

    Layout updated

--- mobile/src/main/java/com/escodro/fysio/activity/CalculatorActivity.java
+++ mobile/src/main/java/com/escodro/fysio/activity/CalculatorActivity.java
@@ -39,7 +39,7 @@ public class CalculatorActivity extends AppCompatActivity implements CalculatorV
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.calculator_main);
+        setContentView(R.layout.fragment_ultrasound);
         mPresenter = new CalculatorPresenterImpl(this);
         initComponents();
     }
